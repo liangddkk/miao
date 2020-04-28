327e2bc9a485ffc7be49c9649fc19707b09dfbb8
liangddkkk <liangddkk@163.com>
2020-04-28 23:06:54 +0800

    liangddkk-lodash.js

--- lodash/liangddkk-lodash.js
+++ lodash/liangddkk-lodash.js
@@ -1,16 +1,50 @@
 var liangddkk = {
-  isNull:function (val){
+  /**
+   * 判断一个数是否为NaN
+   * @param {*} val 
+   * @returns {Boolean}
+   */
+  isNull: function (val){
     if(val === null){
       return true;
     }else{
       return false;
     }
   },
-  isNaN:function(val){
+    /**
+   * 判断一个数字是否为null
+   * @param  {Number} val 
+   * @returns {Boolean}
+   */
+  isNaN: function(val){
     if(val !== val){
       return true;
     }else{
       return false;
     }
+  },
+  /**
+   * 以size拆分成数组块，并返回由这些数组组成的数组
+   * @param {Array} Array
+   * @param {Number} size
+   * @return {Boolean}
+   */
+  chuck: function(array,size = 1){
+    if(array.length == 0){
+      return [];
+    }
+    let ans = [];
+    let cns = 0;
+    ans[0] = [];
+    //创建二维数组
+    ans[0][0] = array[0];
+    for(let i = 1;i < array.length;i++){
+      if(i % size == 0){
+        cns++;
+        ans[cns] = [];
+      }
+      ans[cns][i % size] = array[i];
+    }
+    return ans;
   }
 }
