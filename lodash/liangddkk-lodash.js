0098823b70a5d98816aa4cc2132847911c4bdc14
liangddkkk <liangddkk@163.com>
2020-07-19 21:45:42 +0800

    differenceWith-02

--- lodash/liangddkk-lodash.js
+++ lodash/liangddkk-lodash.js
@@ -152,10 +152,9 @@ var liangddkk = {
     let func = args[args.length-1];
     let ans = [];
     for(let i in array){
-      for(let j in array[i]){
-        if(!func(array[i][j],args[0])){
-          ans.push(array[i][j]);
-        }
+      if(!func(array[i],args[0])){
+        flag = false; 
+        ans.push(array[i]);
       }
     }
     return ans;
